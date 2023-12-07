e74b824012ce3dab31d316c11366f5587929409e
Mesh-254 <meshack1995@gmail.com>
2023-12-07 13:13:58 +0300

    added image section

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    image = FileField('Image', validators=[
+        # FileRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+    ])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
